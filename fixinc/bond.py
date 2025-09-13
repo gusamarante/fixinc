f70e00e0267aaf44ac0b0f370cd97282f428bac6
Gustavo Amarante <gusamarante@gmail.com>
2025-09-13 12:47:59 -0300

    Starting the compound class

--- fixinc/bond.py
+++ fixinc/bond.py
@@ -1,23 +1,32 @@
 import pandas as pd
+from fixinc.daycount import DayCount
 
 
 class Bond:
 
-    def __init__(self, cashflows, reference_date):
-        self.cashflows = cashflows
-
-    def yield2price(self, y):
+    def __init__(self, cashflows, calendar, dcc, yc):
         # TODO Documentation
-        # TODO Daycounts
-        # TODO Compounding
-        pass
+        self.cashflows = cashflows
+        self.dc = DayCount(calendar=calendar, dcc=dcc)
+        self.comp = 1  # TODO Compounder
+
+    def yield2price(self, t, y):
+        # TODO Documentation
+        yf = self.dc.year_fraction(t, self.cashflows.index)
+        disc = 1 / ((1 + y) ** yf)
+        pv = (self.cashflows * disc).sum()
+        return pv
 
 
 
 # ===== EXAMPLE =====
 cf = pd.Series({pd.to_datetime("2026-12-31"): 1000})
-b = Bond(cf, pd.to_datetime('today'))
-print(b.yield2price(0.05))  # Example usage
+b = Bond(
+    cashflows=cf,
+    calendar="us_trading",
+    dcc="act/365",
+)
+print(b.yield2price(t=pd.to_datetime('today').normalize(), y=0.05))
 
 
 
---
+++ fixinc/compounder.py
@@ -0,0 +1,9 @@
+
+
+class RateCompounder:
+
+    def __init__(self, yc='compound'):
+        self.yc = yc
+
+
+# ===== EXAMPLE =====

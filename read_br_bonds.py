697fc86df0d1ffcb2ff3eb0cf75c922bafc25e14
gustavoamarante <gusamarante@gmail.com>
2025-09-14 10:33:06 -0300

    external file

--- read_br_bonds.py
+++ read_br_bonds.py
@@ -8,7 +8,7 @@ file_path = Path(f"/Users/{getuser()}/Dropbox/Lectures/Fixed Income/Data")
 df = pd.DataFrame()
 
 for year in tqdm(range(2003, 2026)):
-    aux = pd.read_csv(file_path.joinpath(f"data_lft {year}.csv"), sep=";")
+    aux = pd.read_csv(file_path.joinpath(f"data_ntnb {year}.csv"), sep=";")
     df = pd.concat([df, aux])
 
 df = df[['maturity', 'bond code']].drop_duplicates(keep='last').sort_values(by='maturity')

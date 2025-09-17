ead4bdf0826a525660363f13b55898b9a66fadbe
Gustavo Amarante <gusamarante@gmail.com>
2025-09-17 20:27:20 -0300

    Add data

--- fixinc/__init__.py
+++ fixinc/__init__.py
@@ -1,9 +1,13 @@
 from fixinc.daycount import DayCount
 from fixinc.compounder import RateCompounder
 from fixinc.bond import Bond
+from fixinc.performance import Performance
+from fixinc.apis import SGS
 
 __all__ = [
     "Bond",
     "DayCount",
+    "Performance",
     "RateCompounder",
+    "SGS",
 ]

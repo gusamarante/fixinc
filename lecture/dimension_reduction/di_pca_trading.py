e6581becf11faefb31ab6cf27381055f75ad76e9
Gustavo Amarante <gusamarante@gmail.com>
2025-10-01 16:51:37 -0300

    Progress

--- lecture/dimension_reduction/di_pca_trading.py
+++ lecture/dimension_reduction/di_pca_trading.py
@@ -39,25 +39,26 @@ di = di.set_index("du")
 # ======================================
 # ===== Charts - Factor timeseries =====
 # ======================================
-# plt.figure(figsize=(size * (16 / 7.3), size))
-#
-# ax = plt.subplot2grid((1, 1), (0, 0))
-# ax.plot(di.index/252, di[["PCDV 1", "PCDV 2", "PCDV 3"]], label=["PCDV 1", "PCDV 2", "PCDV 3"], linewidth=2)
-# ax.axhline(0, color='black', linewidth=0.5)
-# ax.set_title("PCA-DV01 for DI Futures (January Maturities)")
-# ax.set_xlabel("Years to Maturity")
-# ax.tick_params(rotation=90, axis='x')
-# ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
-# ax.xaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
-# ax.legend(frameon=True, loc='lower left')
-#
-#
-# plt.tight_layout()
-# plt.savefig(figure_path.joinpath("Dimensionality Reduction - DI PCDV01.pdf"))
-# plt.show()
-# plt.close()
+plt.figure(figsize=(size * (16 / 7.3), size))
+
+ax = plt.subplot2grid((1, 1), (0, 0))
+ax.plot(di.index/252, di[["PCDV 1", "PCDV 2", "PCDV 3"]], label=["PCDV 1", "PCDV 2", "PCDV 3"], linewidth=2)
+ax.axhline(0, color='black', linewidth=0.5)
+ax.set_title("PCA-DV01 for DI Futures (January Maturities)")
+ax.set_xlabel("Years to Maturity")
+ax.tick_params(rotation=90, axis='x')
+ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
+ax.xaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
+ax.legend(frameon=True, loc='lower left')
 
 
+plt.tight_layout()
+plt.savefig(figure_path.joinpath("Dimensionality Reduction - DI PCDV01.pdf"))
+plt.show()
+plt.close()
+
+
+# Seleção de contratos
 c1, c2, c3 = di.index[1], di["PCDV 3"].idxmax(), di.index[-2]
 
 A = di.loc[[c1, c2, c3], ["PCDV 1", "PCDV 2", "PCDV 3"]].T

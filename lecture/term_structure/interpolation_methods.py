f1878471ad8cdc9116e4dc78053d2b43beb0b602
Gustavo Amarante <gusamarante@gmail.com>
2025-10-23 08:52:08 -0300

    Progress

--- lecture/term_structure/interpolation_methods.py
+++ lecture/term_structure/interpolation_methods.py
@@ -30,7 +30,7 @@ zc = ZeroCurve(
 # print(interp)
 
 mat_range = np.arange(1, 3.01, 0.01)
-fras = pd.DataFrame(
+zc_all_methods = pd.DataFrame(
     {
         "Linear": [zc.interpolate("2025-01-01", t, method="linear") for t in mat_range],
         "Flat Forward": [zc.interpolate("2025-01-01", t, method="flat forward") for t in mat_range],
@@ -38,7 +38,10 @@ fras = pd.DataFrame(
     },
     index=mat_range,
 )
-# TODO, parei aqui, computar os FRAs
+
+fras = pd.DataFrame(columns=zc_all_methods.columns)
+for t2, t1 in zip(zc_all_methods.index[1:], zc_all_methods.index[:-1]):
+    fras.loc[t2] = (((1 + zc_all_methods.loc[t2]) ** t2) / ((1 + zc_all_methods.loc[t1]) ** t1)) ** (1 / (t2 - t1)) - 1
 
 # =================
 # ===== CHART =====
@@ -50,22 +53,19 @@ fig = plt.figure(figsize=(size * (16 / 7.3), size))
 # Curves
 ax = plt.subplot2grid((1, 2), (0, 0))
 ax.plot(
-    mat_range,
-    [zc.interpolate("2025-01-01", t, method="linear") for t in mat_range],
+    zc_all_methods["Linear"],
     label="Linear",
     color=BLUE,
     lw=2,
 )
 ax.plot(
-    mat_range,
-    [zc.interpolate("2025-01-01", t, method="flat forward") for t in mat_range],
+    zc_all_methods["Flat Forward"],
     label="Flat Forward",
     color=RED,
     lw=2,
 )
 ax.plot(
-    mat_range,
-    [zc.interpolate("2025-01-01", t, method="cubic spline") for t in mat_range],
+    zc_all_methods["Cubic Spline"],
     label="Cubic Spline",
     color=GREEN,
     lw=2,
@@ -81,9 +81,31 @@ ax.legend(frameon=True, loc='best')
 
 # Forward Curve
 ax = plt.subplot2grid((1, 2), (0, 1))
+ax.plot(
+    fras["Linear"],
+    label="Linear",
+    color=BLUE,
+    lw=2,
+)
+ax.plot(
+    fras["Flat Forward"],
+    label="Flat Forward",
+    color=RED,
+    lw=2,
+)
+ax.plot(
+    fras["Cubic Spline"],
+    label="Cubic Spline",
+    color=GREEN,
+    lw=2,
+)
 
-
-
+ax.set_title("FRAs of Interpolated Zero Curve")
+ax.set_ylabel("Yields")
+ax.set_xlabel("Maturity (Years)")
+ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
+ax.xaxis.grid(color='grey', linestyle='-', linewidth=0.5, alpha=0.5)
+ax.legend(frameon=True, loc='best')
 
 plt.savefig(figure_path.joinpath("TSIR - Interpolation Methods Example.pdf"))
 plt.tight_layout()

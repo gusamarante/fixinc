16619ac9316ce79cfb3d0f2127c76fb218deeff5
gustavoamarante <gusamarante@gmail.com>
2026-03-29 00:33:01 -0300

    Progress

---
+++ curves/bootstrap_nss.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from fixinc.bond import NTNB
+from data.readers import raw_ntnb
+
+df = raw_ntnb()
+ntnb = NTNB()
+
+dt = df["reference date"].max()
+print(dt)
+
+df = df[df["reference date"] == dt].sort_values("du")
+
+all_cashflows = []
+for year in df["maturity"].dt.year.to_list():
+    aux_cf = ntnb.get_cashflows(dt, year)
+    all_cashflows.append(aux_cf.rename(year))
+
+all_cashflows = pd.concat(all_cashflows, axis=1).fillna(0)
+
+print(all_cashflows)

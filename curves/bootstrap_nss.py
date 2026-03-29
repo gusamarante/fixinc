0c36749f0e24172d66ab62cfa340b0b524291fc9
gustavoamarante <gusamarante@gmail.com>
2026-03-29 18:12:13 -0300

    Progress

--- curves/bootstrap_nss.py
+++ curves/bootstrap_nss.py
@@ -27,32 +27,27 @@ con.execute("""
 """)
 con.commit()
 
-# Use the last stored entry as the warm start, if available
-row = con.execute(
-    "SELECT date, b1, b2, b3, b4, l1, l2 FROM nss_parameters WHERE curve_id = ? ORDER BY date DESC LIMIT 1",
-    (CURVE_ID,)
-).fetchone()
+BETA0 = (0.06, 0.03, 0.0, 0.0)
+LAM0 = (1.2, 0.5)
 
-if row:
-    last_date, *params = row
-    bm1 = tuple(params[:4])
-    lm1 = tuple(params[4:])
-else:
-    last_date = None
-    bm1 = (0.2, 0.2, 0.2, 0.2)
-    lm1 = (1, 1)
+# Find the earliest stored date to know where to resume
+earliest_date = con.execute(
+    "SELECT MIN(date) FROM nss_parameters WHERE curve_id = ?",
+    (CURVE_ID,)
+).fetchone()[0]
 
 df = raw_ntnb()
 ntnb = NTNB()
 dc = DayCount(calendar="anbima", dcc="bus/252", adj="following")
 
-dates2loop = pd.DatetimeIndex(df["reference date"].unique()).sort_values()
-dates2loop = dates2loop[dates2loop >= "2020-01-01"]
-if last_date:
-    dates2loop = dates2loop[dates2loop > last_date]
+dates2loop = pd.DatetimeIndex(df["reference date"].unique()).sort_values(ascending=False)
+# dates2loop = dates2loop[dates2loop >= "2020-01-01"]
+if earliest_date:
+    dates2loop = dates2loop[dates2loop < earliest_date]
 
 for t in tqdm(dates2loop):
-    aux_raw = df[df["reference date"] == t].sort_values("du")
+    print(t)
+    aux_raw = df[df["reference date"] == t].sort_values("du").dropna(subset="yield")
     all_cashflows = []
     prices = pd.Series()
     duration = pd.Series()
@@ -63,14 +58,12 @@ for t in tqdm(dates2loop):
         duration.loc[mat.year] = aux_raw[aux_raw["maturity"] == mat]["modified duration"].iloc[-1]
 
     all_cashflows = pd.concat(all_cashflows, axis=1).fillna(0)
-    bnss = BootstrapNSS(prices, all_cashflows, 1/duration, t, dc, beta0=bm1, lam0=lm1, verbose=False)
+    bnss = BootstrapNSS(prices, all_cashflows, 1/duration, t, dc, beta0=BETA0, lam0=LAM0, verbose=False)
 
     con.execute(
         "INSERT OR REPLACE INTO nss_parameters VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
         (CURVE_ID, t.date().isoformat(), *bnss.beta, *bnss.lam, bnss.sse)
     )
     con.commit()
-    bm1 = bnss.beta
-    lm1 = bnss.lam
 
 con.close()

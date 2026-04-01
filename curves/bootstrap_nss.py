6715576bdbb9b51e49765c1a01cf131849403d40
Gustavo Amarante <gusamarante@gmail.com>
2026-04-01 18:26:13 -0300

    progress

--- curves/bootstrap_nss.py
+++ curves/bootstrap_nss.py
@@ -8,6 +8,9 @@ from fixinc.daycount import DayCount
 CURVE_ID = "ntnb"
 DB_PATH = "../data/nss_parameters.db"
 
+# TODO logic to "fill" dates, starting from the most recent.
+# TODO Logic to "correct" bad estimates.
+
 # Connect to (or create) the database and ensure the table exists
 con = sqlite3.connect(DB_PATH)
 con.execute("""
@@ -29,19 +32,18 @@ con.commit()
 BETA0 = (0.06, 0.03, 0.0, 0.0)
 LAM0 = (1.2, 0.5)
 
-# Find the earliest stored date to know where to resume
-earliest_date = con.execute(
-    "SELECT MIN(date) FROM nss_parameters WHERE curve_id = ?",
-    (CURVE_ID,)
-).fetchone()[0]
-
 df = raw_ntnb()
 ntnb = NTNB()
 dc = DayCount(calendar="anbima", dcc="bus/252", adj="following")
 
-dates2loop = pd.DatetimeIndex(df["reference date"].unique()).sort_values(ascending=False)
-if earliest_date:
-    dates2loop = dates2loop[dates2loop < earliest_date]
+# Find which dates are already in the database
+stored_dates = pd.read_sql_query(
+    "SELECT date FROM nss_parameters WHERE curve_id = ?",
+    con, params=(CURVE_ID,), parse_dates=["date"]
+)["date"]
+
+all_dates = pd.DatetimeIndex(df["reference date"].unique())
+dates2loop = all_dates.difference(stored_dates).sort_values(ascending=False)
 
 for t in dates2loop:
     aux_raw = df[df["reference date"] == t].sort_values("du").dropna(subset="yield")

cde0d90c3afe97e782c1eb952ac3634d49e0c39d
Gustavo Amarante <gusamarante@gmail.com>
2026-04-03 19:08:50 -0300

    progress

--- lecture/term_structure/nss_timeseries.py
+++ lecture/term_structure/nss_timeseries.py
@@ -9,22 +9,28 @@ from utils import figure_path, BLUE
 DB_PATH = "../../data/nss_parameters.db"
 
 con = sqlite3.connect(DB_PATH)
-df = pd.read_sql(
+ntnb = pd.read_sql(
     "SELECT * FROM nss_parameters WHERE curve_id = 'ntnb'",
+    con,
+    index_col="date",
+    parse_dates=["date"],
+)
+ntnf = pd.read_sql(
+    "SELECT * FROM nss_parameters WHERE curve_id = 'ntnf'",
     con,
     index_col="date",
     parse_dates=["date"],
 )
 con.close()
 
-df = df.drop(columns="curve_id")
-df = df.sort_index()
+ntnb = ntnb.drop(columns="curve_id")
+ntnb = ntnb.sort_index()
+
+ntnf = ntnf.drop(columns="curve_id")
+ntnf = ntnf.sort_index()
 
 
-# ===== CHART =====
-size = 5
-fig = plt.figure(figsize=(size * (16 / 7.3), size))
-
+# Chart params
 vars2plot = {
     "b1": {
         "title": r"$\beta_1$",
@@ -64,10 +70,14 @@ vars2plot = {
 }
 
 
+# ===== CHART NTNB =====
+size = 5
+fig = plt.figure(figsize=(size * (16 / 7.3), size))
+
 for var in vars2plot.keys():
     ax = plt.subplot2grid((3, 4), vars2plot[var]["pos"], colspan=vars2plot[var]["colspan"])
     ax.set_title(vars2plot[var]["title"])
-    ax.plot(df[var], color=BLUE)
+    ax.plot(ntnb[var], color=BLUE)
     # ax.axhline(0, color="black", lw=0.5)
     ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
     ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
@@ -79,5 +89,28 @@ for var in vars2plot.keys():
 plt.tight_layout()
 
 plt.savefig(figure_path.joinpath("NSS - Brazil NTNB Parameters.pdf"))
+plt.show()
+plt.close()
+
+
+# ===== CHART NTNF =====
+size = 5
+fig = plt.figure(figsize=(size * (16 / 7.3), size))
+
+for var in vars2plot.keys():
+    ax = plt.subplot2grid((3, 4), vars2plot[var]["pos"], colspan=vars2plot[var]["colspan"])
+    ax.set_title(vars2plot[var]["title"])
+    ax.plot(ntnf[var], color=BLUE)
+    # ax.axhline(0, color="black", lw=0.5)
+    ax.xaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5)
+    ax.yaxis.grid(color="grey", linestyle="-", linewidth=0.5, alpha=0.5, which="both")
+    locators = mdates.YearLocator()
+    ax.xaxis.set_major_locator(locators)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+    ax.tick_params(rotation=90, axis="x")
+
+plt.tight_layout()
+
+plt.savefig(figure_path.joinpath("NSS - Brazil NTNF Parameters.pdf"))
 plt.show()
 plt.close()

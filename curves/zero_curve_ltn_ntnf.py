396d46f773cf642e400b01ea0bdec0ac50a07386
Gustavo Amarante <gusamarante@gmail.com>
2026-08-05 21:40:37 -0300

    Finished the nominal bonds bootstrap

--- curves/zero_curve_ltn_ntnf.py
+++ curves/zero_curve_ltn_ntnf.py
@@ -1,12 +1,12 @@
 from data.readers import raw_ltn_ntnf
-from fixinc import Bootstrap, LTN, NTNF
+from fixinc import Bootstrap, LTN, NTNF, DayCount
 import pandas as pd
 from tqdm import tqdm
 
 
 bltn = LTN()
 bntn = NTNF()
-
+dc = DayCount(calendar="anbima", dcc="bus/252")
 
 df = raw_ltn_ntnf()
 
@@ -17,7 +17,7 @@ df["bond type"] = bc
 
 
 all_dates = sorted(df["reference date"].unique())
-
+all_curves = []
 for t in tqdm(all_dates):
 
     df_aux = df[df["reference date"] == t].set_index("bond code")
@@ -28,26 +28,30 @@ for t in tqdm(all_dates):
 
     for bc in df_aux.index:
 
-        if df_aux.loc[bc, "bond type"] == "LTN":
-            cf = bltn.get_cashflows(
-                t=df_aux.loc[bc, "reference date"],
-                mat=df_aux.loc[bc, "maturity"].strftime("%Y-%m"),
-            )
+        try:
+            if df_aux.loc[bc, "bond type"] == "LTN":
+                cf = bltn.get_cashflows(
+                    t=df_aux.loc[bc, "reference date"],
+                    mat=df_aux.loc[bc, "maturity"].strftime("%Y-%m"),
+                )
 
-        elif df_aux.loc[bc, "bond type"] == "NTNF":
-            cf = bntn.get_cashflows(
-                t=df_aux.loc[bc, "reference date"],
-                mat=df_aux.loc[bc, "maturity"].year,
-            )
+            elif df_aux.loc[bc, "bond type"] == "NTNF":
+                cf = bntn.get_cashflows(
+                    t=df_aux.loc[bc, "reference date"],
+                    mat=df_aux.loc[bc, "maturity"].year,
+                )
 
-        else:
-            raise ValueError("Unknown bond type")
+            else:
+                raise ValueError("Unknown bond type")
+
+        except AssertionError:
+            continue
 
         all_cashflows.append(cf.rename(bc))
         all_prices.loc[bc] = df_aux.loc[bc, "price"]
         all_duration.loc[bc] = df_aux.loc[bc, "duration"]
 
-    all_cashflows = pd.concat(all_cashflows, axis=1).fillna(0).sort_index()
+    all_cashflows = pd.concat(all_cashflows, axis=1, sort=False).fillna(0).sort_index()
 
 
     boot = Bootstrap(
@@ -58,5 +62,12 @@ for t in tqdm(all_dates):
     )
 
     yc = boot.get_zero_curve("anbima", "bus/252", "compound")
+    yc.index = dc.days(t, yc.index)
+    all_curves.append(yc.rename(t))
 
-    a = 1
+
+all_curves = pd.concat(all_curves, axis=1).sort_index(axis=0).sort_index(axis=1).T
+print(all_curves)
+
+# TODO interpolate, select months and save
+# TODO why was I doing this again?
